6abc828bc52cd1e3f739806c75b080fc5f28c141
Astika Luintel <astild2020@gmail.com>
2023-09-21 18:19:09 -0400

    soln

---
+++ recursive_count.rb
@@ -0,0 +1,15 @@
+def recursive_count(num = 0)
+  return if num >= 10
+  puts num 
+
+    recursive_count(num + 1)
+  # type your code in here
+end
+
+if __FILE__ == $PROGRAM_NAME
+  recursive_count
+end
+
+# OPTIONAL
+# Please add your pseudocode to this file
+# And a written explanation of your solution
